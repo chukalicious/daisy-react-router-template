f9a05193e52c3fea72da254b0926b53600f4d5a2
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-05-12 21:10:38 -0700

    chore: Add About page and update Home page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./components/Home";
+import About from "./pages/About";
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<h1>404</h1>} />
       </Route>)
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -7,22 +8,11 @@ const Navbar = () => {
                 <a className="btn btn-ghost text-xl">Vite React daisyUI App</a>
             </div>
             <div className="flex-none">
-
-                <button className="btn btn-square btn-ghost">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="inline-block w-5 h-5 stroke-current"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                        ></path>
-                    </svg>
-                </button>
+                <Link to="about">
+                    <button className="btn btn-square btn-ghost">
+                        About
+                    </button>
+                </Link>
             </div>
 
         </div>
